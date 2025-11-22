69a4780b3661c8efda08a5b9ccbfc2c5161b899a
RytAakarsh <aakarshmishra70@gmail.com>
2025-11-23 01:04:34 +0530

    files updated

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const path = require('path');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",    // or your real frontend domain when deployed
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
+
 app.use(express.json());
 
 // Routes
